7cea6495f04ad25fea80751c1737d11400e277fe
pomlink-mena <105843131+pomlink-mena@users.noreply.github.com>
2022-05-18 23:44:52 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$("#typed").typed({
+  		strings: ["Carolina", "Liz", "Michael"],
+  		typeSpeed: 100,
+  		startDelay: 0,
+  		backSpeed: 60,
+  		backDelay: 2000,
+  		loop: true,
+  		cursorChar: "|",
+  		contentType: 'html'
+  	});
